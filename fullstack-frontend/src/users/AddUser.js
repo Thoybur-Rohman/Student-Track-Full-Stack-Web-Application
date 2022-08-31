d8ae421b2d635419179d24b9490859d57917465e
Anti Coding Club <73062879+Thoybur-Rohman@users.noreply.github.com>
2022-08-31 12:20:03 +0100

    Update

--- fullstack-frontend/src/users/AddUser.js
+++ fullstack-frontend/src/users/AddUser.js
@@ -1,24 +1,64 @@
-
-import React from 'react'
+import React, {useState} from 'react'
+import Button from "bootstrap/js/src/button";
 
 export default function AddUser() {
-  return (
-    <div className="container">      
-      <div className="row">
-        <div classNam="row">
-        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-          <h2 className="text-center m-4">Register User</h2>
-          <div className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Name
-            </label>
-              </div>
+
+  const [user,setUser] = useState({
+    name:"",
+    email:"",
+    username:""
+  })
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div classNam="row">
+                    <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                        <h2 className="text-center m-4">Register User</h2>
+                        <div className="mb-3">
+                            <label htmlFor="Name" className="form-label">
+                                Name
+                            </label>
+                            <input
+                                type={"text"}
+                                className={"form-control"}
+                                placeholder={"Enter a name "}/>
+                        </div>
 
 
+                        <div className="mb-3">
+                            <label htmlFor="Email" className="form-label">
+                                Email
+                            </label>
+                            <input
+                                type={"text"}
+                                className={"form-control"}
+                                placeholder={"Enter your Email "}/>
+                        </div>
+
+
+                        <div className="mb-3">
+                            <label htmlFor="Username" className="form-label">
+                                Username
+                            </label>
+                            <input
+                                type={"text"}
+                                className={"form-control"}
+                                placeholder={"Enter a User name "}/>
+                        </div>
+
+                      <button type="submit" className="btn btn-outline-primary">
+                        Submit
+                      </button>
+                      <button type="submit" className="btn btn-outline-danger mx-2">
+                        Cancel
+                      </button>
+
+
+                    </div>
+
+                </div>
+            </div>
         </div>
-
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
